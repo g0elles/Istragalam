fc59eee255bb6bd8654c74e12afc4e8f2be86b75
g0elles <ellesgabry5@gmail.com>
2019-04-17 19:05:45 -0500

    Organizacion de elementos y una que otra cosa mas, ademas se definieron los iconos de la barra superior

--- app/src/main/java/cuc/edu/co/istragalam/Home/HomeActivity.java
+++ app/src/main/java/cuc/edu/co/istragalam/Home/HomeActivity.java
@@ -7,7 +7,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import Utils.BottomNavigationViewHelper;
@@ -21,8 +23,30 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: starting.");
         setUtbottomNavigationView();
+        setupViewPager();
+    }
+
+    /**
+     * Responsible for adding the 3 tabs: Camera, Home, Messages
+     */
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment());
+        adapter.addFragment(new HomeFragment());
+        adapter.addFragment(new MessageFragment());
+        ViewPager viewPager = findViewById(R.id.container);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_action_name);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
+
+
 
     }
+
+
     /*
      * BottomNavigationView setup
      * **/
