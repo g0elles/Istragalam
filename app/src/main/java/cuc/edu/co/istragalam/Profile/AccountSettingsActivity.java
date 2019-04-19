45f5fad6c2140d4ac6427ac44c7876174a1fe0e8
g0elles <ellesgabry5@gmail.com>
2019-04-19 12:46:04 -0500

    se agregaron mas vistas como menu de opciones del perfil y el perfil en si

--- app/src/main/java/cuc/edu/co/istragalam/Profile/AccountSettingsActivity.java
+++ app/src/main/java/cuc/edu/co/istragalam/Profile/AccountSettingsActivity.java
@@ -2,9 +2,15 @@ package cuc.edu.co.istragalam.Profile;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 import cuc.edu.co.istragalam.R;
 
@@ -15,7 +21,33 @@ public class AccountSettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accountsettings);
         Log.d(TAG, "onCreate:  started");
+        setupSettingslist();
+
+        //la barra para volveer al perfil
+        ImageView backArrow =  findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating back to 'ProfileActivity'");
+                finish();
+            }
+        });
+
+
         
+    }
+    
+    private void setupSettingslist(){
+        Log.d(TAG, "setupSettingslist: inicio account settings list");
+        ListView listView = findViewById(R.id.lvAccountSettings);
+        ArrayList <String> options =  new ArrayList<>();
+        options.add(getString(R.string.edit_profile));
+        options.add(getString(R.string.sing_out));
+
+
+        ArrayAdapter adapter = new ArrayAdapter(AccountSettingsActivity.this, android.R.layout.simple_list_item_1, options);
+        listView.setAdapter(adapter);
+
     }
 }
 
--- app/src/main/java/cuc/edu/co/istragalam/Profile/ProfileActivity.java
+++ app/src/main/java/cuc/edu/co/istragalam/Profile/ProfileActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,11 +22,14 @@ public class ProfileActivity extends AppCompatActivity {
     private static final ProfileActivity ourInstance = new ProfileActivity();
     private static final String TAG = "ProfileActivity";
     private static final int ACTIVITY_NUM = 4;
+    private ProgressBar mProgressBar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: Started");
+        mProgressBar = findViewById(R.id.profileProgressBar);
+        mProgressBar.setVisibility(View.GONE);
         setUtbottomNavigationView();
         setupToolbar();
     }
