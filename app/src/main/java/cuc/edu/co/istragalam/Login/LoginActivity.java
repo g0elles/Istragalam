6c9629f733fa40d31a29238b203f71510af22f36
Gab <ellesgabry5@gmail.com>
2019-04-25 18:24:35 -0500

    inicio de sesion pronto a estar listo y se agrego firebasae

---
+++ app/src/main/java/cuc/edu/co/istragalam/Login/LoginActivity.java
@@ -0,0 +1,20 @@
+package cuc.edu.co.istragalam.Login;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import cuc.edu.co.istragalam.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Log.d(TAG, "onCreate: started");
+    }
+}
--- app/src/main/java/cuc/edu/co/istragalam/Login/RegisterActivity.java
+++ app/src/main/java/cuc/edu/co/istragalam/Login/RegisterActivity.java
@@ -1,4 +1,4 @@
-package cuc.edu.co.istragalam.Likes;
+package cuc.edu.co.istragalam.Login;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -13,14 +13,14 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import Utils.BottomNavigationViewHelper;
 import cuc.edu.co.istragalam.R;
 
-public class LikesActivity extends AppCompatActivity {
-    private static final LikesActivity ourInstance = new LikesActivity();
+public class RegisterActivity extends AppCompatActivity {
+    private static final RegisterActivity ourInstance = new RegisterActivity();
     private static final String TAG = "LikesActivity";
     private static final int ACTIVITY_NUM = 3;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_register);
         Log.d(TAG, "onCreate: Started");
          setUtbottomNavigationView();
 
@@ -33,7 +33,7 @@ public class LikesActivity extends AppCompatActivity {
         Log.d(TAG, "setUtbottomNavigationView: setting up bottomNavigationView");
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewHelper.enableNavigation(LikesActivity.this, bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(RegisterActivity.this, bottomNavigationViewEx);
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
