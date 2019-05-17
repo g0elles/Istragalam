2293a84b38086115d9072bb9c7d4759d86631ef6
Gab <ellesgabry5@gmail.com>
2019-05-17 00:43:15 -0500

    ya funciona el inicio de sesion, ahora se  debe hacer el edit de los datos de la cuenta

--- app/src/main/java/cuc/edu/co/istragalam/Home/HomeActivity.java
+++ app/src/main/java/cuc/edu/co/istragalam/Home/HomeActivity.java
@@ -1,17 +1,27 @@
 package cuc.edu.co.istragalam.Home;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.Arrays;
 
 import Utils.BottomNavigationViewHelper;
 import Utils.SectionsPagerAdapter;
@@ -21,6 +31,11 @@ import cuc.edu.co.istragalam.R;
 public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "HomeActivity";
     private static final int ACTIVITY_NUM = 0;
+    //firebase
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    public static final int RC_SIGN_IN = 1;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
@@ -28,6 +43,35 @@ public class HomeActivity extends AppCompatActivity {
         initImageLoader();
         setUtbottomNavigationView();
         setupViewPager();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user!= null){
+                    //onSignedInInitialize(user.getDisplayName());
+                }else{
+                    //onSignetOutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.FacebookBuilder().build(),
+                                            new AuthUI.IdpConfig.EmailBuilder().build()
+
+                                     ))
+                                    .setTheme(R.style.LoginTheme)
+                                    .setLogo(R.drawable.istragalam_logo)
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+
+
     }
     private void initImageLoader(){
         UniversalmageLoader universalImageLoader = new UniversalmageLoader(HomeActivity.this);
@@ -69,4 +113,31 @@ public class HomeActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "pelao te logueasate", Toast.LENGTH_SHORT).show();
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
 }
